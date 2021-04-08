7de7f125680457555107c57af3a89e45f7f8872d
won <rjsdnql123@gmail.com>
2021-04-08 14:26:40 +0900

    Use useState to reRendering 리 렌더이 안되는 오류 useState로 수정

--- src/view/TodoView.js
+++ src/view/TodoView.js
@@ -3,21 +3,37 @@ import React,{useState} from 'react';
 function TodoView (props) {
     let todos = props.viewModel.getAll()
     const [inputs, setInput] = useState('')
+    const [toggleState, setToggleState] = useState(true)
+
+    const remove = idx => {
+        props.viewModel.remove(idx)
+        setToggleState(!toggleState)
+    }
+    const onToggle = idx => {
+        props.viewModel.onToggle(idx)
+        setToggleState(!toggleState)
+    }
+    const add = () => {
+        props.viewModel.add(inputs)
+        setInput('')
+        setToggleState(!toggleState)
+    }
+
     return (
       <>
         <div className="form">
-          <input type="text" placeholder="등록" onChange={(e) => setInput(e.target.value)}/>
-          <button className="create" onClick={(e) => props.viewModel.add(inputs)}>등록</button>
+          <input type="text" placeholder="등록" value={inputs} onChange={(e) => setInput(e.target.value)}/>
+          <button className="create" onClick={add}>등록</button>
         </div>
         <div className="list">
           <ul>
             {todos.map( (todo, idx) => (
               <li key={idx} >
-                <span onClick={() => props.viewModel.onToggle(idx)}>
+                <span onClick={() => onToggle(idx)}>
                 {todo.check === true ? <span style={{color:'gray'}}>{todo.todos}</span> : <span>{todo.todos}</span>}
                 </span>
                 <button 
-                  onClick={() => props.viewModel.remove(idx)}
+                  onClick={() => remove(idx)}
                   style={{color: 'red'}}
                 >
                   삭제
